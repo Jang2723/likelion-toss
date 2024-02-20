657bdebc28ab9ec1a69e5a4690900915780450b4
Jang2723 <jangsb46@gmail.com>
2024-02-20 15:03:41 +0900

    오타 수정

--- src/main/java/com/example/toss/TossController.java
+++ src/main/java/com/example/toss/TossController.java
@@ -5,7 +5,10 @@ import com.example.toss.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
@@ -20,7 +23,7 @@ public class TossController {
             @RequestBody
             PaymentConfirmDto dto
     ) {
-        log.info("received: {}",dto.toString());
+        log.info("received: {}", dto.toString());
         return service.confirmPayment(dto);
     }
 }
